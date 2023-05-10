6b4bafe7be4879ae64ee95a31577fb91659d19de
邱旭 <uxuip@outlook.com>
2023-05-10 16:32:43 +0800

    chore: 自动导入

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from 'vite'
 import Uni from '@dcloudio/vite-plugin-uni'
 import Unocss from 'unocss/vite'
 import Eslint from 'vite-plugin-eslint'
+import AutoImport from 'unplugin-auto-import/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -17,5 +18,13 @@ export default defineConfig({
     // see unocss.config.ts for config
     Unocss(),
     Eslint(),
+    // https://github.com/antfu/unplugin-auto-import
+    AutoImport({
+      imports: [
+        'vue',
+      ],
+      dts: true,
+      vueTemplate: true,
+    }),
   ],
 })
